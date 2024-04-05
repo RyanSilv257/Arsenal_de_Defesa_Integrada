5e09de35eed1bee3f8b0fc2a256dd8f5ddbd9ebb
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2024-04-05 14:14:52 -0300

    dd

--- site/src/App.js
+++ site/src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/header'
+import Header from './components/header';
+import Footer from './components/footer'
 
 function App() {
   return (
@@ -10,10 +11,11 @@ function App() {
     <>
     <head>
     <style>
-@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap')
-</style>
+      @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap')
+    </style>
 </head>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ site/src/components/footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer(){
+    return (
+        <>
+    
+        </>
+    )
+}
+
+export default Footer;
