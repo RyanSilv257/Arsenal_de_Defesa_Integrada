01d856dd0f167b6b78a584eb94391ce085839d8f
RyanSilv257 <130668947+RyanSilv257@users.noreply.github.com>
2024-04-12 08:19:56 -0300

    commit

--- site/src/App.js
+++ site/src/App.js
@@ -5,20 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
 import Footer from './components/footer';
 import Home from './components/home';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+
 
 function App() {
   return (
     
-    <>
+    <BrowserRouter>
     <head>
     <style>
       @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap')
     </style>
 </head>
+
       <Header />
       <Home />
       <Footer />
-    </>
+
+    </BrowserRouter>
   );
 }
 
---
+++ site/src/Sobre/Sobre.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About(){
+    return(
+        <>
+            <h1>Oi</h1>
+            <h1>Oi</h1>
+            <h1>Oi</h1>
+        </>
+    )
+}
