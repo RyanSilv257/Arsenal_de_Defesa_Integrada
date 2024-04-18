81161c523be5cd029c782d28ea9dc80ea07f3c1e
Gui1304 <130668551+Gui1304@users.noreply.github.com>
2024-04-18 09:06:40 -0300

    commit Guilherme

--- site/src/components/Armas.jsx
+++ site/src/components/Armas.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import "./Armas.css";
 
-function Armas(){
-    return(
-        <>
-            <h1>Teste</h1>
-        </>
-    );
+function Armas() {
+  return (
+    <div className="armas-container">
+      <h1>Armas</h1>
+      <div className="arma">
+        <h2>Nome da Arma</h2>
+        <p></p>
+        <img src="PISTOLA_TAURUS.jpg" alt="Pistola Taurus" />
+      </div>
+      {
+
+      }
+    </div>
+  );
 }
 
 export default Armas;
