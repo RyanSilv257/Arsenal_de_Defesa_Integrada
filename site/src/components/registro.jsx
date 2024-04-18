be69daa9caa72fbc6e9390b266a278602bdc1d8e
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2024-04-18 10:00:32 -0300

    asdf

--- site/src/components/registro.jsx
+++ site/src/components/registro.jsx
@@ -149,16 +149,16 @@ function Registro(){
                 <hr />
                 </div>
 
-                <div class="low-bar">
+                <div class="low-b4r">
                 <p>CNPJ: 27.779.008/0001-54.</p>
                 <p>TEL: (32) 96025-3648.</p>
                 <p>ADI (Arsenal de Defesa Integrada )<br/>  ADIarsenal.com.br, Inc. ou suas  © 2024.</p>
             </div>
-            <div class="low-bar2">
+            <div class="low-b4r2">
                 <p class ="content-txt3">Rua Acutirembiú,Jardim São Sebastião,<br/> São Paulo, SP,<br/> CEP: 08041-090.</p>
                 <img class="content-img2" src="/18.png" alt="Proíbido para menores de 18 anos." />
             </div>
-            <div class="low-bar3">
+            <div class="low-b4r3">
                 <p class ="content-txt3">Formas de pagamento aceitas: cartões de crédito 
                 (Visa, MasterCard, Elo e American Express), cartões de débito (Visa e Elo), Boleto e Pix.</p>
             </div>
