61aa4bc7becffd588e4ac698da321af79eff1439
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2024-04-19 13:58:14 -0300

    commit

--- site/src/components/login.jsx
+++ site/src/components/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { Link } from "react-router-dom";
 
 function Login(){
 
@@ -33,28 +33,82 @@ function Login(){
 
     return(
         <>
+
+        <div class="main-div">
+            <div class="t-bar">
+                <p class="logo-txt1" >ADI</p>
+                <img class="img-hd" src="/logo_header2.png"/>
+                <p class="logo-txt2">Arsenal de Defesa Integrada</p>
+            </div>  
+            <div class="txt1">
+                <h1 class="p">Entrar:</h1>    
+            </div>
+
+
             <form onSubmit={handleLogin}
             >
-            <input
+                <div class="div1">
+                <p class="category1">Email:</p>
+            <input className="email-inp"
             type="email"
             name="email"
-            placeholder="email"
+            placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
             />
-            <input
+            </div>
+                <div class="div3">
+                <p class="category">Senha:</p>
+            <input className="senha-inp"
             type="password"
             name="senha"
-            placeholder="senha"
+            placeholder="Senha"
             required
             value={senha} 
             onChange={(e) => setSenha(e.target.value)}
             />
-            <button type="submit">
+            </div>
+            <div class="cnt">
+            <div class="btn-s">
+                        <Link to="/">
+                        <button className="form_button1">Voltar ao ínicio</button>
+                        </Link>
+            </div>
+            <div class="btn-1">
+            <button className="form_button2" type="submit">
             Log in
             </button>
+            </div>
+            </div>
+            <div class="txt2">
+                    <p>Não tem uma conta?</p>
+                </div>
+                <div class="href">
+                    <Link to="/registro">
+                        <a class="ent">Registre-se</a>
+                    </Link>
+                </div>
             </form>
+            <div class="hr">
+                <hr />
+                </div>
+
+                <div class="low-b4r">
+                <p>CNPJ: 27.779.008/0001-54.</p>
+                <p>TEL: (32) 96025-3648.</p>
+                <p>ADI (Arsenal de Defesa Integrada )<br/>  ADIarsenal.com.br, Inc. ou suas  © 2024.</p>
+            </div>
+            <div class="low-b4r2">
+                <p class ="content-txt3">Rua Acutirembiú,Jardim São Sebastião,<br/> São Paulo, SP,<br/> CEP: 08041-090.</p>
+                <img class="content-img2" src="/18.png" alt="Proíbido para menores de 18 anos." />
+            </div>
+            <div class="low-b4r3">
+                <p class ="content-txt3">Formas de pagamento aceitas: cartões de crédito 
+                (Visa, MasterCard, Elo e American Express), cartões de débito (Visa e Elo), Boleto e Pix.</p>
+            </div>
+        </div>
+
         </>
     )
 }
--- site/src/components/registro.jsx
+++ site/src/components/registro.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import{ useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 import "../styles/registro.css";
 import { Link } from "react-router-dom";
 
@@ -36,78 +35,6 @@ function Registro(){
     };
 
 
-
-
-
-
-    // const [result, setResult] = useState ([]);
-    // const [dataToInsert, setDataToInsert] = useState({
-    // nome: "",
-    // email: "",
-    // senha: ""
-    // });
-    // const [redirected, setRedirected ] = useState(false);
-    
-    // const navigate = useNavigate();
-
-    // useEffect(() => {
-    //     fetch("http://localhost:3000")
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             setResult(data);
-            
-    //             // Procura o item com o mesmo ProductID que o pathname.
-    //             const foundItem = data.find(
-    //             (item) => window.location.pathname === `/modify/${item.id}`
-    //             );
-    
-    //             if (foundItem) {
-    //             setDataToInsert((prevState) => ({
-    //             ...prevState,
-    //             ...foundItem,
-    //     }));
-    // } else {
-    //         // Se não encontrar o item, redireciona para a página principal.
-    //         if (!redirected) {
-    //             setRedirected(true);
-    //             navigate("/Registro");
-    //          }
-    //     }   
-    // })
-    // .catch((err) => { 
-    //   console.error(err);
-    //     });
-    // },[]);
-
-    // // Envia as informações para o backend quando o botão de enviar é clicado.
-    // const handleSubmit = (e) => {
-    //     const foundItem = result.find(
-    // (item) => window.location.pathname === `/modify/${item.id}`
-    // );
-    // if (foundItem) {
-    //   fetch("http://localhost:3000", {
-    //     method: "PUT",
-    //     body: JSON.stringify(dataToInsert),
-    //     headers: {"Content-Type": "application/json"},
-    //     });
-    //     navigate("/Registro")
-    //     } else {
-    //         fetch("http://localhost:3000", {
-    //         method: "POST",
-    //         body: JSON.stringify(dataToInsert),
-    //         headers: {"Content-Type": "application/json"},
-    //         });
-    //     }
-    // };
-    // // Armazena as informações no estado conforme são digitados.
-    // const handleChange = (e) => {
-    //     setDataToInsert({
-    //     ...dataToInsert,
-    //     [e.target.name]: e.target.value,
-    //     });
-    // };
-
-
     return (
         <>
         <div class="main-div">
@@ -117,7 +44,7 @@ function Registro(){
                 <p class="logo-txt2">Arsenal de Defesa Integrada</p>
             </div>  
             <div class="txt1">
-                <p class="p">Registre-se:</p>    
+                <h1 class="p">Registre-se:</h1>    
             </div>  
    
             <form onSubmit={handleRegistro}>
@@ -149,7 +76,7 @@ function Registro(){
                 <div class="div3">
                 <p class="category">Senha:</p>
                 <input className="senha-inp"
-                    type="text"
+                    type="password"
                     value={senha}
                     name="senha"
                     onChange={(e) => setSenha(e.target.value)}
