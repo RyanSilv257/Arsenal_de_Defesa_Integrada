52cbe82553ece92b44c1bd35aaa3964966bc16b7
HenriqueMiaciro <130669699+HenriqueMiaciro@users.noreply.github.com>
2024-04-19 14:02:35 -0300

    commit

--- site/src/components/licensas.jsx
+++ site/src/components/licensas.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "../styles/licensas.css";
-import "../components/header";
-import "../components/footer";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 function licensas() {
     return(
-        <div>
-            
+        <div className="container">
+            <Header title="Licenses" />
+            <h1>Licenses Page</h1>
+            <p>Here is some information about the licenses for this project.</p>
+            <Footer />
         </div>
     );
 };
+
+export default licensas;
