633a1249cd29d09f93b003cf7c03c3910593d6ed
José Luiz <130668873+zeluiz9571@users.noreply.github.com>
2024-04-25 09:03:10 -0300

    .dyftujh
    utfvkutyvutcrszrf

--- site/src/components/Armas.jsx
+++ site/src/components/Armas.jsx
@@ -1,9 +1,9 @@
 import'../styles/geral.css';
-import  produto1 from "../Produtos/Machado Tático.jpeg"
-import  produto2 from "../Produtos/Canivete suiço tatico multifuncional.jpeg"
-import  produto3 from "../Produtos/Canivete Tático Militar Camuflado R$ 149,00.jpeg"
-import  produto4 from "../Produtos/CANIVETE TÁTICO MILITAR LANTERNA PEDERNEIRA BORLOV NAUTIKA AÇO INOX SOBREVIVENCIA R$ 88,26.jpeg"
-import  produto5 from "../Produtos/CANIVETE TÁTICO MILITAR SEMI AUTOMÁTICO XD-13 R$ 167,87.jpeg"
+import  produto1 from "../Produtos/PISTOLA TAURUS 838C CALIBRE .380ACP.jpeg"
+import  produto2 from "../Produtos/PISTOLA STOEGER STR9 CAL 9MM.jpeg"
+import  produto3 from "../Produtos/RIFLE .22 SEMIAUTOMÁTICO TACTICAL CORONHA TÁTICA EM POLIPROPILENO PRETA- CBC R$3.652,00.jpeg"
+import  produto4 from "../Produtos/REVÓLVER RT 065 TAURUS CAL 357 MAG.jpeg"
+import  produto5 from "../Produtos/RIFLE CBC PUMP ACTION GALERY CAL 22LR CORONHA DE MADEIRA.jpeg"
 import  produto6 from "../Produtos/Canivete Tático Shark Green Militar Original Invictus R$118,90.jpeg"
 import  produto7 from "../Produtos/CANIVETE TORO SOCO INGLÊS TÁTICO NAUTIKA R$ 147,00.jpeg"
 import  produto8 from "../Produtos/Ferramenta multiuso tática Spor TAG em aço inox R$ 62,90.jpeg"
@@ -19,7 +19,7 @@ function Armas(){
     <header>
     <div class='sub-div'>
         <div class='sub-title'>
-            <h1 id='title1'>CUTELARIA</h1>
+            <h1 id='title1'>ARMAMENTO</h1>
             <hr />
         </div>
      </div>   
@@ -28,36 +28,36 @@ function Armas(){
     <section>
         <div class="product">
             <img src={produto1} alt="Acessório de caça 1"/>
-            <h2>Machado Tático</h2>
-            <p>R$197,00</p>
+            <h2>PISTOLA TAURUS 838C CALIBRE .380ACP</h2>
+            <p>R$8.089,90</p>
             <button>Adicionar ao carrinho</button>
         </div>
 { /*____________________________________________2____________________________________________________________*/ }
         <div class="product">
             <img src={produto2} alt="Acessório de caça 2"/>
-            <h2>Canivete suiço tatico multifuncional</h2>
-            <p>$179,90</p>
+            <h2>PISTOLA STOEGER STR9 CAL 9MM</h2>
+            <p>R$8.559,00</p>
             <button>Adicionar ao carrinho</button>
         </div>
  { /*___________________________________________3_____________________________________________________________*/ }
         <div class="product">
             <img src={produto3} alt="Acessório de caça 3"/>
-            <h2>Canivete Tático Militar Camuflado</h2>
-            <p>R$149,00</p>
+            <h2>RIFLE .22 SEMIAUTOMÁTICO TACTICAL CORONHA TÁTICA EM POLIPROPILENO PRETA- CBC</h2>
+            <p>R$3.652,00</p>
             <button>Adicionar ao carrinho</button>
         </div>
 { /*____________________________________________4____________________________________________________________*/ }
         <div class="product">
             <img src={produto4} alt="Acessório de caça 4"/>
-            <h2>CANIVETE TÁTICO MILITAR LANTERNA PEDERNEIRA BORLOV NAUTIKA AÇO INOX SOBREVIVENCIA</h2>
-            <p>R$88,26</p>
+            <h2>REVÓLVER RT 065 TAURUS CAL 357 MAG</h2>
+            <p>R$4590,00</p>
             <button>Adicionar ao carrinho</button>
         </div>
 { /*____________________________________________5____________________________________________________________*/ }
     <div class="product">
         <img src={produto5} alt="Acessório de caça 5"/>
-        <h2>CANIVETE TÁTICO MILITAR SEMI AUTOMÁTICO XD-13</h2>
-        <p>R$167,87</p>
+        <h2>RIFLE CBC PUMP ACTION GALLERY CAL 22LR CORONHA DE MADEIRA</h2>
+        <p>R$5.849,90</p>
         <button>Adicionar ao carrinho</button>
     </div>
 { /*____________________________________________6____________________________________________________________*/ }
