1f970c3b6ae0e269257fb4a310f0ae410919434b
José Luiz <130668873+zeluiz9571@users.noreply.github.com>
2024-04-26 12:08:36 -0300

    aaaaaaaaaaaaaaaaaaa
    aaaaaaaaaaaaaaaaaaaaaaaaa

--- site/src/components/Cutelaria.jsx
+++ site/src/components/Cutelaria.jsx
@@ -1,13 +1,13 @@
 import'../styles/geral.css';
-import  produto1 from "../Produtos/Machado Tático.jpeg"
-import  produto2 from "../Produtos/Canivete suiço tatico multifuncional.jpeg"
-import  produto3 from "../Produtos/Canivete Tático Militar Camuflado R$ 149,00.jpeg"
-import  produto4 from "../Produtos/CANIVETE TÁTICO MILITAR LANTERNA PEDERNEIRA BORLOV NAUTIKA AÇO INOX SOBREVIVENCIA R$ 88,26.jpeg"
-import  produto5 from "../Produtos/CANIVETE TÁTICO MILITAR SEMI AUTOMÁTICO XD-13 R$ 167,87.jpeg"
-import  produto6 from "../Produtos/Canivete Tático Shark Green Militar Original Invictus R$118,90.jpeg"
-import  produto7 from "../Produtos/CANIVETE TORO SOCO INGLÊS TÁTICO NAUTIKA R$ 147,00.jpeg"
-import  produto8 from "../Produtos/Ferramenta multiuso tática Spor TAG em aço inox R$ 62,90.jpeg"
-import  produto9 from "../Produtos/Faca Armada NTK Tático Empunhadura Soco-inglês Militar Caça R$ 208,90.jpeg"
+import  produto13 from "../Produtos/Machado Tático.jpeg"
+import  produto14 from "../Produtos/Canivete suiço tatico multifuncional.jpeg"
+import  produto15 from "../Produtos/Canivete Tático Militar Camuflado R$ 149,00.jpeg"
+import  produto16 from "../Produtos/CANIVETE TÁTICO MILITAR LANTERNA PEDERNEIRA BORLOV NAUTIKA AÇO INOX SOBREVIVENCIA R$ 88,26.jpeg"
+import  produto17 from "../Produtos/CANIVETE TÁTICO MILITAR SEMI AUTOMÁTICO XD-13 R$ 167,87.jpeg"
+import  produto18 from "../Produtos/Canivete Tático Shark Green Militar Original Invictus R$118,90.jpeg"
+import  produto19 from "../Produtos/CANIVETE TORO SOCO INGLÊS TÁTICO NAUTIKA R$ 147,00.jpeg"
+import  produto20 from "../Produtos/Ferramenta multiuso tática Spor TAG em aço inox R$ 62,90.jpeg"
+import  produto21 from "../Produtos/Faca Armada NTK Tático Empunhadura Soco-inglês Militar Caça R$ 208,90.jpeg"
 
 /*_____________________________________________________________________________________________________*/
 function Cutelaria(){
@@ -29,63 +29,63 @@ function Cutelaria(){
     
     <section>
         <div class="product">
-            <img src={produto1} alt="Acessório de caça 1"/>
+            <img src={produto13} alt="Acessório de caça 1"/>
             <h2>Machado Tático</h2>
             <p>R$197,00</p>
             <button>Adicionar ao carrinho</button>
         </div>
 { /*____________________________________________2____________________________________________________________*/ }
         <div class="product">
-            <img src={produto2} alt="Acessório de caça 2"/>
+            <img src={produto14} alt="Acessório de caça 2"/>
             <h2>Canivete suiço tatico multifuncional</h2>
             <p>$179,90</p>
             <button>Adicionar ao carrinho</button>
         </div>
  { /*___________________________________________3_____________________________________________________________*/ }
         <div class="product">
-            <img src={produto3} alt="Acessório de caça 3"/>
+            <img src={produto15} alt="Acessório de caça 3"/>
             <h2>Canivete Tático Militar Camuflado</h2>
             <p>R$149,00</p>
             <button>Adicionar ao carrinho</button>
         </div>
 { /*____________________________________________4____________________________________________________________*/ }
         <div class="product">
-            <img src={produto4} alt="Acessório de caça 4"/>
+            <img src={produto16} alt="Acessório de caça 4"/>
             <h2>CANIVETE TÁTICO MILITAR LANTERNA PEDERNEIRA BORLOV NAUTIKA AÇO INOX SOBREVIVENCIA</h2>
             <p>R$88,26</p>
             <button>Adicionar ao carrinho</button>
         </div>
 { /*____________________________________________5____________________________________________________________*/ }
     <div class="product">
-        <img src={produto5} alt="Acessório de caça 5"/>
+        <img src={produto17} alt="Acessório de caça 5"/>
         <h2>CANIVETE TÁTICO MILITAR SEMI AUTOMÁTICO XD-13</h2>
         <p>R$167,87</p>
         <button>Adicionar ao carrinho</button>
     </div>
 { /*____________________________________________6____________________________________________________________*/ }
     <div class="product">
-        <img src={produto6} alt="Acessório de caça 6"/>
+        <img src={produto18} alt="Acessório de caça 6"/>
         <h2>Canivete Tático Shark Green Militar Original Invictus R$118,90</h2>
         <p>R$118,90</p>
         <button>Adicionar ao carrinho</button>
     </div>
 { /*____________________________________________7____________________________________________________________*/ }
     <div class="product">
-        <img src={produto7} alt="Acessório de caça 7"/>
+        <img src={produto19} alt="Acessório de caça 7"/>
         <h2>CANIVETE TORO SOCO INGLÊS TÁTICO NAUTIKA R$ 147,00.jpeg</h2>
         <p>R$147,00</p>
         <button>Adicionar ao carrinho</button>
     </div>
 { /*_____________________________________________8___________________________________________________________*/ }
     <div class="product">
-        <img src={produto8} alt="Acessório de caça 8"/>
+        <img src={produto20} alt="Acessório de caça 8"/>
         <h2>Ferramenta multiuso tática Spor TAG em aço inox</h2>
         <p>R$62,90</p>
         <button>Adicionar ao carrinho</button>
     </div>
 { /*_____________________________________________9___________________________________________________________*/ }
     <div class="product">
-        <img src={produto9} alt="Acessório de caça 8"/>
+        <img src={produto21} alt="Acessório de caça 8"/>
         <h2>Faca Armada NTK Tático Empunhadura Soco-inglês Militar Caça</h2>
         <p>R$208,90</p>
         <button>Adicionar ao carrinho</button>
