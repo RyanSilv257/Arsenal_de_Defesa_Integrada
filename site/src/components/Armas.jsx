683b9be530697f1e56d856a2f5cf9775ea1dc9a3
José Luiz <130668873+zeluiz9571@users.noreply.github.com>
2024-05-03 09:47:48 -0300

    dwfvvfeversv
    gvgqr3gvbabbr

--- site/src/components/Armas.jsx
+++ site/src/components/Armas.jsx
@@ -44,77 +44,99 @@ function Armas(){
             <img src={produto2} alt="Acessório de caça 2"/>
             <h2>PISTOLA TAURUS 838C CALIBRE .380ACP</h2>
             <p>R$8.559,00</p>
+            <Link to="/Produto">
             <button>Adicionar ao carrinho</button>
+            </Link>
         </div>
  { /*___________________________________________3_____________________________________________________________*/ }
         <div class="product">
             <img src={produto3} alt="Acessório de caça 3"/>
             <h2>RIFLE .22 SEMIAUTOMÁTICO TACTICAL CORONHA TÁTICA EM POLIPROPILENO PRETA- CBC</h2>
             <p>R$3.652,00</p>
+            <Link to="/Produto">
             <button>Adicionar ao carrinho</button>
+            </Link>
         </div>
 { /*____________________________________________4____________________________________________________________*/ }
         <div class="product">
             <img src={produto4} alt="Acessório de caça 4"/>
             <h2>REVÓLVER RT 065 TAURUS CAL 357 MAG</h2>
             <p>R$4590,00</p>
+            <Link to="/Produto">
             <button>Adicionar ao carrinho</button>
+            </Link>
         </div>
 { /*____________________________________________5____________________________________________________________*/ }
     <div class="product">
         <img src={produto5} alt="Acessório de caça 5"/>
         <h2>RIFLE CBC PUMP ACTION GALLERY CAL 22LR CORONHA DE MADEIRA</h2>
         <p>R$5.849,90</p>
+        <Link to="/Produto">
         <button>Adicionar ao carrinho</button>
+        </Link>
     </div>
 { /*____________________________________________6____________________________________________________________*/ }
     <div class="product">
         <img src={produto6} alt="Acessório de caça 6"/>
         <h2>Carabina Rossi Puma Tática .357 MAG - Preta</h2>
         <p>R$9.690,00</p>
-        <button>Adicionar ao carrinho</button>
+        <Link to="/Produto">
+            <button>Adicionar ao carrinho</button>
+        </Link>
     </div>
 { /*____________________________________________7____________________________________________________________*/ }
     <div class="product">
         <img src={produto7} alt="Acessório de caça 7"/>
         <h2>REVÓLVER RT066 TAURUS CAL 357MAG</h2>
         <p>R$5.099,00</p>
-        <button>Adicionar ao carrinho</button>
+        <Link to="/Produto">
+            <button>Adicionar ao carrinho</button>
+        </Link>
     </div>
 { /*_____________________________________________8___________________________________________________________*/ }
     <div class="product">
         <img src={produto8} alt="Acessório de caça 8"/>
         <h2>Carabina Winchester 22 Modelo 70 Featherweight Compact MID Calibre 22-250</h2>
         <p>R$2.890,00</p>
-        <button>Adicionar ao carrinho</button>
+        <Link to="/Produto">
+            <button>Adicionar ao carrinho</button>
+        </Link>
     </div>
 { /*_____________________________________________9___________________________________________________________*/ }
     <div class="product">
         <img src={produto9} alt="Acessório de caça 9"/>
         <h2>Carabina Winchester 22 Modelo 70 Featherweight Compact MID Calibre 22-250</h2>
         <p>R$2.890,00</p>
-        <button>Adicionar ao carrinho</button>
+        <Link to="/Produto">
+            <button>Adicionar ao carrinho</button>
+        </Link>
     </div>
 { /*_____________________________________________10___________________________________________________________*/ }
     <div class="product">
         <img src={produto10} alt="Acessório de caça 10"/>
         <h2>ESPINGARDA CBC PUMP MILITARY 3.0 19POL CAL 12</h2>
         <p>R$8.299,90</p>
-        <button>Adicionar ao carrinho</button>
+        <Link to="/Produto">
+            <button>Adicionar ao carrinho</button>
+        </Link>
     </div>
 { /*_____________________________________________11___________________________________________________________*/ }
     <div class="product">
         <img src={produto11} alt="Acessório de caça 11"/>
         <h2>PISTOLA TAURUS TH380 CALIBRE .380ACP</h2>
         <p>R$8.149,90</p>
-        <button>Adicionar ao carrinho</button>
+        <Link to="/Produto">
+            <button>Adicionar ao carrinho</button>
+        </Link>
     </div>
     { /*_____________________________________________12___________________________________________________________*/ }
     <div class="product">
         <img src={produto12} alt="Acessório de caça 12"/>
         <h2>PISTOLA TAURUS TS9C GRAPHENE</h2>
         <p>R$9.549,00</p>
-        <button>Adicionar ao carrinho</button>
+        <Link to="/Produto">
+            <button>Adicionar ao carrinho</button>
+        </Link>
     </div>
     </section>
         </>
