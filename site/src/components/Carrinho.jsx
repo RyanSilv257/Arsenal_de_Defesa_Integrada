129db062d846282a309c93e94f7eca1ebdd3ca9e
HenriqueMiaciro <130669699+HenriqueMiaciro@users.noreply.github.com>
2024-05-03 13:54:20 -0300

    commit

--- site/src/components/Carrinho.jsx
+++ site/src/components/Carrinho.jsx
@@ -1,1 +1,44 @@
-import'../styles/geral.css';
+import React, { useEffect, useState } from 'react';
+import { Commerce } from 'commercejs';
+
+
+const commerce = new Commerce(process.env.REACT_APP_PUBLIC_KEY);
+
+function Carrinho() {
+  const [cart, setCart] = useState({});
+
+  useEffect(() => {
+    commerce.cart.retrieve().then(setCart);
+  }, []);
+
+  const handleUpdateCartQty = (lineItemId, quantity) => {
+    commerce.cart.update(lineItemId, { quantity }).then(setCart);
+  };
+
+  const handleRemoveCartItem = (lineItemId) => {
+    commerce.cart.remove(lineItemId).then(setCart);
+  };
+
+  return (
+    <div>
+      {cart.line_items && (
+        <ul>
+          {cart.line_items.map((item) => (
+            <li key={item.id}>
+              <div>{item.product.name}</div>
+              <div>{item.quantity}</div>
+              <button onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}>-</button>
+              <button onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}>+</button>
+              <button onClick={() => handleRemoveCartItem(item.id)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <div>
+        Total: ${cart.total_price}
+      </div>
+    </div>
+  );
+}
+
+export default Carrinho;
